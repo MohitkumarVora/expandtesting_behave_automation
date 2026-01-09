96df9153e8645463ebdd995fd65e9ad710b6772f
Mohitkumar Vora <mohitv88@gmail.com>
2026-01-09 15:20:40 +0530

    login page automation and add incognito mode code

--- features/pages/base_page.py
+++ features/pages/base_page.py
@@ -5,44 +5,131 @@ from features.configs.config_reader import ConfigReader
 
 
 class BasePage:
+    """
+    BasePage contains all common reusable Selenium actions.
+    All Page Object classes should inherit from this class.
+    """
 
     def __init__(self, driver):
+        """
+        Constructor to initialize WebDriver and explicit wait.
+
+        :param driver: Selenium WebDriver instance
+        """
         self.driver = driver
         config = ConfigReader()
         explicit_wait = config.get_explicit_wait()
         self.wait = WebDriverWait(driver, explicit_wait)
 
     def wait_for_element_visible(self, locator):
+        """
+        Wait until the element is present in DOM and visible on UI.
+
+        :param locator: Tuple(By, locator)
+        :return: WebElement
+        """
         return self.wait.until(EC.visibility_of_element_located(locator))
 
     def wait_for_element_clickable(self, locator):
+        """
+        Wait until the element is visible and enabled so it can be clicked.
+
+        :param locator: Tuple(By, locator)
+        :return: WebElement
+        """
         return self.wait.until(EC.element_to_be_clickable(locator))
 
     def click(self, locator):
+        """
+        Click on an element.
+        First attempts normal Selenium click.
+        Falls back to JavaScript click if normal click fails.
+
+        :param locator: Tuple(By, locator)
+        """
         element = self.wait_for_element_clickable(locator)
+
+        # Scroll element into view to avoid interception issues
         self.driver.execute_script(
             "arguments[0].scrollIntoView({block: 'center'});", element
         )
-        self.driver.execute_script("arguments[0].click();", element)
+
+        try:
+            element.click()
+        except:
+            # JS click fallback for stubborn elements
+            self.driver.execute_script("arguments[0].click();", element)
 
     def double_click_action(self, locator):
+        """
+        Perform double-click action on an element.
+
+        :param locator: Tuple(By, locator)
+        """
         element = self.wait_for_element_visible(locator)
         ActionChains(self.driver).double_click(element).perform()
 
     def right_click_action(self, locator):
+        """
+        Perform right-click (context click) on an element.
+
+        :param locator: Tuple(By, locator)
+        """
         element = self.wait_for_element_visible(locator)
         ActionChains(self.driver).context_click(element).perform()
 
     def type(self, locator, text):
+        """
+        Clear existing text and type new text into input field.
+
+        :param locator: Tuple(By, locator)
+        :param text: Text to be entered
+        """
         element = self.wait_for_element_visible(locator)
         element.clear()
         element.send_keys(text)
 
     def get_text(self, locator):
+        """
+        Get visible text from an element.
+
+        :param locator: Tuple(By, locator)
+        :return: Text value of the element
+        """
         return self.wait_for_element_visible(locator).text
 
     def get_attribute(self, locator, attribute):
+        """
+        Get attribute value of an element.
+
+        :param locator: Tuple(By, locator)
+        :param attribute: Attribute name
+        :return: Attribute value
+        """
         return self.wait_for_element_visible(locator).get_attribute(attribute)
 
     def scroll_down(self, pixels=300):
+        """
+        Scroll down the page by given pixels.
+
+        :param pixels: Number of pixels to scroll (default 300)
+        """
         self.driver.execute_script(f"window.scrollBy(0, {pixels})")
+
+    def is_element_visible(self, locator):
+        """
+        Check if element is visible on UI.
+        Returns True if visible, False otherwise.
+
+        :param locator: Tuple(By, locator)
+        :return: Boolean
+        """
+        try:
+            self.wait_for_element_visible(locator)
+            return True
+        except:
+            return False
+    
+    def wait_for_url_contains(self, text):
+        self.wait.until(EC.url_contains(text))
+    
--- features/pages/login_page.py
+++ features/pages/login_page.py
@@ -4,16 +4,41 @@ from features.pages.base_page import BasePage
 
 class LoginPage(BasePage):
 
-    # Locators
+    # -------- Locators --------
     link_test_login_page = (By.LINK_TEXT, "Test Login Page")
-    username = (By.ID, "username")
-    password = (By.ID, "password")
+    username_input = (By.ID, "username")
+    password_input = (By.ID, "password")
     login_btn = (By.XPATH, "//button[text()='Login']")
+    success_message = (By.XPATH, "//div[(@id='flash') and contains(@class,'success')]")
+    error_message = (By.XPATH, "//div[@id='flash']/b")
+    logout_btn = (By.XPATH, "//a[@href='/logout']")
 
+    # -------- Page Actions --------
     def open(self):
+        """Open Test Login Page from home"""
         self.click(self.link_test_login_page)
 
-    def login(self, user, pwd):
-        self.type(self.username, user)
-        self.type(self.password, pwd)
+    def enter_username(self, username):
+        self.type(self.username_input, username)
+
+    def enter_password(self, password):
+        self.type(self.password_input, password)
+
+    def click_login(self):
         self.click(self.login_btn)
+
+    def login(self, user, pwd):
+        """Single-step login (optional usage)"""
+        self.enter_username(user)
+        self.enter_password(pwd)
+        self.click_login()
+
+    # -------- Validations --------
+    def get_success_message(self):
+        return self.get_text(self.success_message)
+
+    def get_error_message(self):
+        return self.get_text(self.error_message)
+
+    def is_logout_button_displayed(self):
+        return self.is_element_visible(self.logout_btn)
